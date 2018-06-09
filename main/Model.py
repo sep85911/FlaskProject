488a2422d4c5f8ae61ee9ac17cb7415ff785dd3a
sep85911 <sep85911@outlook.com>
2018-06-09 23:31:12 +0800

    201806092331

---
+++ main/Model.py
@@ -0,0 +1,16 @@
+from flaskApp import db;
+
+class User(db.Model):
+    __tablename__ = 'user';
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True);
+    username = db.Column(db.String(100),nullable = False);
+    age = db.Column(db.Integer, autoincrement=True);
+
+class Article(db.Model):
+    __tablename__ = 'article';
+    id = db.Column(db.Integer,primary_key = True,autoincrement = True);
+    title = db.Column(db.String(100),nullable = False );
+    content = db.Column(db.Text, nullable = False );
+    author_id = db.Column(db.Integer,db.ForeignKey('user.id'));
+    author = db.relationship('User', backref=db.backref('articles'))
+
