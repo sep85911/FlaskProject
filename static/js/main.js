c33ac481c043eb848d5e72adf26ed4ca0a404eb2
Micah <sep85911@163.com>
2018-06-25 09:29:27 +0800

    201806250929

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,28 @@
-console.log("hello web")
-
-
 
 function TestFun001(){
-    var t = document.getElementById('id001')
-    t.innerHTML = "fuck you"
+    var str001 = 'string001'
+
+    console.log(str001[1]);
+
+    var today = new Date()        
+    var d1 = new Date("September 11, 1985 04:00:00")        
+    var d2 = new Date(79,5,24)    //年月日    
+    var d3 = new Date(79,5,24,11,33,0) //年月日时分秒
+
+
+    console.log(today)
+    console.log(d1)
+    console.log(d2)
+    console.log(d3)
+
+
+    console.log(screen.availHeight);
+    console.log(screen.availWidth);
+    console.log(screen.height);
+    console.log(screen.width);
+    console.log(navigator.platform);
+
+
+    
 }
+
