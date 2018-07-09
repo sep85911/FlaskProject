da8c49f3ca74e41a43f5c338d6a0c23ac309aa39
Micah <sep85911@163.com>
2018-07-09 17:19:01 +0800

    201807091718

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,8 @@
+$(document).ready(function(){
 
-function TestFun001(){
-    var str001 = 'string001'
+    var btn = $("button");
 
-    console.log(str001[1]);
-
-    var today = new Date()        
-    var d1 = new Date("September 11, 1985 04:00:00")        
-    var d2 = new Date(79,5,24)    //年月日    
-    var d3 = new Date(79,5,24,11,33,0) //年月日时分秒
-
-
-    console.log(today)
-    console.log(d1)
-    console.log(d2)
-    console.log(d3)
-
-
-    console.log(screen.availHeight);
-    console.log(screen.availWidth);
-    console.log(screen.height);
-    console.log(screen.width);
-    console.log(navigator.platform);
-
-
-    
-}
-
+    btn.click(function(){
+        $("#form1").hide();
+    })
+})
