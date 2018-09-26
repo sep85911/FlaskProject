2f6fd70732b473d11eb1e0bab84bd1766b513aa6
Micah <sep85911@outlook.com>
2018-09-26 22:26:30 +0800

    201809262226

--- main/Test.py
+++ main/Test.py
@@ -1,3 +1,26 @@
 import sys;
 
 print(sys.version);
+
+sStr1 = 'fdsfsaffdsfs';
+
+sStr2 = "string no2";
+
+
+print(sStr1 + sStr2); #python 字符串连接用的是+
+
+
+iList1 = [1,2,3,4]  #list 用的是方括号[]
+iList2 = [5,6,7];
+
+print(iList1 + iList2);
+iList1[-1] = 54
+print(iList1[3]);
+
+tTuple = (1,2,3,4,5)    #tuple 用的是圆括号()
+
+print(tTuple[0]);
+
+tDict = {"a":11,"b":22,"fuck":1314}
+
+print(tDict["fuck"])
