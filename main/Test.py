c7b3f74a451638539e46122e17fff12da4764463
Micah <sep85911@outlook.com>
2019-01-13 16:58:45 +0800

    201901131658

--- main/Test.py
+++ main/Test.py
@@ -2,21 +2,68 @@ import sys;
 import os;
 import re;
 
+import string;
+import datetime;
+import calendar;
+import time;
+
+import tkinter;
+from tkinter import Listbox;
+from tkinter import messagebox;
+
+
 # 脚本所在文件夹
 Script_folder = "E:\\xydII\\WorldServer\\Script";
 
-def CheckTaskID(address):
-    for filename in os.listdir(Script_folder):
-        print(filename);
+# def CheckTaskID(address):
+#     for filename in os.listdir(Script_folder):
+#         print(filename);
 
 
+def DelMore(nums):
+    length = len(nums);
+
+    index = 0;
+
+    while(index + 1 < length):
+        start = index;
+        for i in range( index + 1, length):
+            if nums[start] == nums[i]:
+                del nums[i];
+                # index += 1;
+                length = len(nums);
+                break;
+            else:
+                if i == length - 1:
+                    index += 1;
+                    break;
+    return len(nums);
+
+def showmessage(a,b,c):
+    print("测试中：") 
+
+    
+
+
+
+    
 if __name__ == "__main__":
-    # CheckTaskID(Script_folder);
+    
+    top = tkinter.Tk();    
 
-    testStr = '"12345"';
+    screenW = top.winfo_screenwidth();
+    screenH = top.winfo_screenheight();
+    selfW = 100;
+    selfH = 60
+    size = "%dx%d+%d+%d" %( selfW,selfH,(screenW - selfW) / 2, (screenH - selfH) / 2 );
+    top.geometry(size);
+
+    b = tkinter.Button(top, text="测试",command=lambda : showmessage(a=1, b=2, c=3))    
+    b.pack(expand="yes");
 
 
-    myre = re.compile("\d+",flags=0);
+    print("按下测试键开始...")
+    top.mainloop();
 
-    myre.search(testStr);
-    print(testStr);
+
+   
--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
     var btn = $("button");
 
     btn.click(function(){
+        console.log("31231212312")
         $("#form1").hide();
     })
 })
+
+
+
+
+
