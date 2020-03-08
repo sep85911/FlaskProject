71c1c39158cf823b42180f8bfec2cce355b64ea0
Micah <sep85911@outlook.com>
2020-03-08 23:24:40 +0800

    202003082324

--- App/main/Test3.py
+++ App/main/Test3.py
@@ -1,38 +1,63 @@
+import glob;
 
 
-
-class myClass:
-    id = -1;
+class CObject:
+    id = 0;
     name = "noname";
 
     def __init__(self):
-        print("类myClass 初始化。。。");
         self.id = 0;
         self.name = "init name";
 
-    def __print(self):
-        print(self.id);
-        print(self.name);
+    def GetID(self):
+        return self.id;
 
-    def output(self):
+    def GetName(self):
+        return self.name;
 
-        self.__print();
-        print(self.id);
-        print(self.name);
+class CCreature(CObject):
 
-class myMonster(myClass):
-    skillid = 0;
+    hp = 100;
+    mp = 100;
+    sp = 100;
 
-    def __init__(self):
-        myClass.__init__(self);
-        print("类myMonster 初始化。。。");
-        skillid = 1;
+    def funcname(self):
+        pass
+
+    
+
+
+    # def __init__(self):
+    #     myClass.__init__(self);
+    #     print("类myMonster 初始化。。。");
+    #     skillid = 1;
+
+import os;
+import calendar;
+import shutil;
+import glob;
+import random;
+
+import lxml;
+
+import venv;
 
 
 if __name__ == "__main__":
     
 
-    m = myMonster();
+    # print(glob.glob(os.curdir + "/*.*"));
 
-    print(m.name);
+    #yy = int(input("请输入年份:"));
+    #mm = int(input("请输入月份:"));
+
+    noRepeatList = random.sample([i * 10 for i in range(10)],6);#采样 取不重复的n项
+
+    print(noRepeatList)
+
+    # 显示日历
+
+    # print(calendar.month(yy,mm));
+
+
 
