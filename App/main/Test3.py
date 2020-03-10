7a8e25a0606d135a69d44872aaeab031f39d53ce
Micah <sep85911@outlook.com>
2020-03-10 23:05:46 +0800

    202003102303

--- App/main/Test3.py
+++ App/main/Test3.py
@@ -2,18 +2,36 @@ import glob;
 
 
 class CObject:
-    id = 0;
-    name = "noname";
+    _id = 0;
+    _name = "noname";
 
     def __init__(self):
-        self.id = 0;
-        self.name = "init name";
+        self._id = 0;
+        self._name = "init name";
 
-    def GetID(self):
-        return self.id;
+    @property
+    def ID(self):
+        return self._id;
 
-    def GetName(self):
-        return self.name;
+    @property
+    def Name(self):
+        return self._name;
+
+    @ID.setter
+    def ID(self, id):
+        if id > 10000:
+            print("id太大了，无法设置");
+            return;
+
+        self._id = id;
+
+    @Name.setter
+    def Name(self,name):
+        if name == "fuck":
+            print("脏话！无法设置");
+            return;
+        
+        self._name = name;
 
 class CCreature(CObject):
 
@@ -43,17 +61,38 @@ import lxml;
 import venv;
 
 
+def log(text):
+    def decorator(func):
+        def wrapper(*args,**kw):
+            print("This is %s" % text);
+            return func(*args,**kw);
+    
+        return wrapper;
+    return decorator;
+
+@log("first print")
+def testfunc():
+    print("This is testfunc");
+
 if __name__ == "__main__":
     
+    obj001 = CObject();
 
+    obj001.ID = 99999;
+
+    print(obj001.ID);
+
+    obj001.ID = 1234;
+
+    print(obj001.ID)
     # print(glob.glob(os.curdir + "/*.*"));
 
     #yy = int(input("请输入年份:"));
     #mm = int(input("请输入月份:"));
 
-    noRepeatList = random.sample([i * 10 for i in range(10)],6);#采样 取不重复的n项
+    # noRepeatList = random.sample([i * 10 for i in range(10)],6);#采样 取不重复的n项
 
-    print(noRepeatList)
+    # print(noRepeatList)
 
     # 显示日历
 
